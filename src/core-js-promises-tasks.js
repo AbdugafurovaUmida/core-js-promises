bfd75e5f236a463c3793b8f12c1f01e5d590d827
AbdugafurovaUmida <abdugafurova93@gmail.com>
2024-04-15 00:02:39 +0500

    feat: resolved queuPromises function

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -150,8 +150,20 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return new Promise((resolve) => {
+    let result = '';
+
+    const nextPromise = (index = 0) => {
+      if (index === promises.length) resolve(result);
+      promises[index].then((data) => {
+        result += data;
+        nextPromise(index + 1);
+      });
+    };
+
+    nextPromise();
+  });
 }
 
 module.exports = {
